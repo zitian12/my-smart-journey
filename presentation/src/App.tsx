4ea5742b40c3ea662501758cbcb08a67f6928d35
Jasonyappp <jasonyap102204@gmail.com>
2026-08-05 12:26:16 +0800

    update mainpage

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
-import { DashboardPage } from "./pages/DashboardPage";
-import { DestinationsPage } from "./pages/DestinationsPage";
 import { Home } from "./pages/Home";
-import { ItineraryPage } from "./pages/ItineraryPage";
 
 export default function App() {
   return (
@@ -11,9 +8,6 @@ export default function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="destinations" element={<DestinationsPage />} />
-          <Route path="itinerary" element={<ItineraryPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
